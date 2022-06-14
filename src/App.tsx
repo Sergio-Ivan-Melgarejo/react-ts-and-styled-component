02f82d72c59ac29e05fe204e60f12dd51b9da40d
Sergio Ivan Melgarejo <sergio.ivan.melgarejo@gmail.com>
2022-06-14 13:35:24 -0300

    basic project structure added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+interface AppProps {
+  title:string
+}
 
-function App() {
+interface Task {
+  id:number,
+  title:string,
+  decription:string,
+  completed:boolean
+}
+
+function App({ title }: AppProps) {
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      id:1,
+      title: "Learn React",
+      decription: "Learn React",
+      completed: false
+    },
+    {
+      id:2,
+      title: "Learn Typescript",
+      decription: "Learn Typescript",
+      completed: false
+    },
+    {
+      id:3,
+      title: "Learn Styled Component",
+      decription: "Learn Styled Component",
+      completed: false
+    }
+  ])
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          {title}
+        </h1>
       </header>
+      {
+        tasks.map(task => (
+          <div key={task.id}>
+            <h2>{task.title}</h2>
+            <p>{task.decription}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App title="React with Tipescript and Styled Component" />
   </React.StrictMode>
 );
 
